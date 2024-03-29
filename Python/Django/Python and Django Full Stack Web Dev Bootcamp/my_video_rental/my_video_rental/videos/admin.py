931852362a13690d1bc239e1a36c01ec6050211f
jumiduss <64105705+jumiduss@users.noreply.github.com>
2024-03-29 07:56:03 -0400

    Add files via upload

---
+++ Python/Django/Python and Django Full Stack Web Dev Bootcamp/my_video_rental/my_video_rental/videos/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+
+class MovieAdmin(admin.ModelAdmin):
+        fields = ['release_year','title','length']
+        search_fields = ['title','length']
+        list_filter = ['release_year','length','title']
+        list_display = ['release_year','length','title']
+        
+admin.site.register(models.Customer)
+admin.site.register(models.Movie)
